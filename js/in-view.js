c32ecb691803c5c5037b628f2a7a928815d4ac84
Mao Yamamura <ymmrmo@yahoo.co.jp>
2015-10-26 00:59:57 +0900

    変更　秒

--- js/in-view.js
+++ js/in-view.js
@@ -2,13 +2,40 @@ $(function () {
     $('.about').css('opacity', 0);
     $('.about').on('inview', function(event, isInView, visiblePartX, visiblePartY) {
         if (isInView) {
-                $('.about').stop().animate({opacity: 1}, 1000);
+                $('.about').stop().animate({opacity: 1}, 500);
         }else {
-    $('.about').css('opacity', 0);
+            $('.about').stop().animate({opacity: 0}, 500);
         }
     });
     $('.contentsWrapper').css('opacity', 0);
     $('.contentsWrapper').on('inview', function(event, isInView, visiblePartX, visiblePartY) {
+        if (isInView) {
+            $(this).stop().animate({opacity: 1}, 500);
+        }
+        else {
+            $(this).stop().animate({opacity: 0}, 500);
+        }
+    });
+    $('.leap').css('opacity', 0);
+    $('.leap').on('inview', function(event, isInView, visiblePartX, visiblePartY) {
+        if (isInView) {
+            $(this).stop().animate({opacity: 1}, 900);
+        }
+        else {
+            $(this).stop().animate({opacity: 0}, 900);
+        }
+    });
+    $('.inoue').css('opacity', 0);
+    $('.inoue').on('inview', function(event, isInView, visiblePartX, visiblePartY) {
+        if (isInView) {
+            $(this).stop().animate({opacity: 1}, 900);
+        }
+        else {
+            $(this).stop().animate({opacity: 0}, 900);
+        }
+    });
+    $('.yamada').css('opacity', 0);
+    $('.yamada').on('inview', function(event, isInView, visiblePartX, visiblePartY) {
         if (isInView) {
             $(this).stop().animate({opacity: 1}, 900);
         }
